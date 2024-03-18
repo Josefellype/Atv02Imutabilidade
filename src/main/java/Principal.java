dc76fda6da3f69b91fce69ac3dbd0dd5f855d127
Josefellype <124213188+Josefellype@users.noreply.github.com>
2024-03-18 17:38:19 -0300

    Update Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,9 +1,19 @@
+import exercicios.*;
 /**
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
 public class Principal {
     public static void main(final String[] args) {
+        final var cidade = new Cidade("Palmas");
+        final var pessoa = new PessoaRecordShallow("José", cidade);
 
+        pessoa.setCidade(new Cidade("Miracema")); //ERRO
+        pessoa.cidade().setNome("Miracema");
+
+        final var cr = new CidadeRecord("Palmas");
+        final var pr = new PessoaRecord("Manoel",cr);
+        pr.setCidade(new Cidade("Miracema")); //ERRO
+        pr.cidade.setNome("Paraíso"); //ERRO
     }
 }
